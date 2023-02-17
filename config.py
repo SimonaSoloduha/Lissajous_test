cf494c8de4675106586efb982157fc3232e7208e
Ivanov Ivan <no_mail@email.com>
2023-02-17 11:33:50 +0300

    add fix

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# Настройки фигуры по умолчанию
 DEFAULT_SETTINGS = {
     "freq_x": 5,
     "freq_y": 1,
@@ -6,7 +5,6 @@ DEFAULT_SETTINGS = {
     "width": 1,
 }
 
-# цвета
 COLORS = {
     "Красный": "crimson",
     "Зелёный": "navy",
@@ -14,5 +12,4 @@ COLORS = {
     "Синий": "midnightblue"
 }
 
-# версия
 VERSION = '0.1.2023.02'
